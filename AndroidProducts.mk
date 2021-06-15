93c1fedf202c33da6750f48b4f65b31bec6d4680
Abhinav Verma <77457859+abhinavftp98@users.noreply.github.com>
2021-06-15 14:52:20 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/corvus_RMX1805.mk
+    $(LOCAL_DIR)/evolution_RMX1805.mk
 
 COMMON_LUNCH_CHOICES := \
-    corvus_RMX1805-user \
-    corvus_RMX1805-userdebug \
-    corvus_RMX1805-eng
+    evolution_RMX1805-user \
+    evolution_RMX1805-userdebug \
+    evolution_RMX1805-eng
