9570bdc73588dedd928022ecdd3a411082025d57
Abhinav Verma <77457859+abhinavftp98@users.noreply.github.com>
2021-06-22 19:10:05 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/corvus_RMX1805.mk
+    $(LOCAL_DIR)/revengeos_RMX1805.mk
 
 COMMON_LUNCH_CHOICES := \
-    corvus_RMX1805-user \
-    corvus_RMX1805-userdebug \
-    corvus_RMX1805-eng
+    revengeos_RMX1805-user \
+    revengeos_RMX1805-userdebug \
+    revengeos_RMX1805-eng
