289a52e61b0ec051925c73409c3aa695dcd21822
Subas <77564932+subas57@users.noreply.github.com>
2021-07-18 15:02:21 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/evolution_RMX1805.mk
+    $(LOCAL_DIR)/dot_RMX1805.mk
 
 COMMON_LUNCH_CHOICES := \
-    evolution_RMX1805-user \
-    evolution_RMX1805-userdebug \
-    evolution_RMX1805-eng
+    dot_RMX1805-user \
+    dot_RMX1805-userdebug \
+    dot_RMX1805-eng
