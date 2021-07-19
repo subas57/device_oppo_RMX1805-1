8eb68a1c5060d3e3c585ca21ab9455da2427e4a5
Subas <77564932+subas57@users.noreply.github.com>
2021-07-19 12:10:54 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/dot_RMX1805.mk
+    $(LOCAL_DIR)/aosp_RMX1805.mk
 
 COMMON_LUNCH_CHOICES := \
-    dot_RMX1805-user \
-    dot_RMX1805-userdebug \
-    dot_RMX1805-eng
+    aosp_RMX1805-user \
+    aosp_RMX1805-userdebug \
+    aosp_RMX1805-eng
